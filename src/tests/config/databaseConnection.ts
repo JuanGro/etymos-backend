b18f7fdc4fcb587ec4514d6996b1ad83434e943e
Juan Guerrero <ing.juangro@gmail.com>
2021-06-18 17:26:14 -0500

    Try new travis config

--- src/tests/config/databaseConnection.ts
+++ src/tests/config/databaseConnection.ts
@@ -2,20 +2,12 @@ import "reflect-metadata";
 import { createConnection, getConnection } from "typeorm";
 
 export function dbConnection() {
-  beforeAll(async () => {
-    try {
-      await createConnection();
-    } catch (error) {
-      throw new Error(error);
-    }
+  beforeAll(() => {
+    return createConnection().then(() => { }).catch((error) => { throw new Error(error) });
   });
 
-  afterAll(async () => {
-    try {
-      const testingConnection = getConnection();
-      await testingConnection.close();
-    } catch (error) {
-      throw new Error(error);
-    }
+  afterAll(() => {
+    const testingConnection = getConnection();
+    return testingConnection.close().then(() => { }).catch((error) => { throw new Error(error) });
   });
 }
