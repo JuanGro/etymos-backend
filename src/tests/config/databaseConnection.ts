f45368ace92f76f157bcf58bab8225cc4c2675da
Juan Guerrero <ing.juangro@gmail.com>
2021-06-18 15:42:28 -0500

    Fix problem with tests db connection

--- src/tests/config/databaseConnection.ts
+++ src/tests/config/databaseConnection.ts
@@ -2,12 +2,14 @@ import "reflect-metadata";
 import { createConnection, getConnection } from "typeorm";
 
 export function dbConnection() {
-  beforeAll(async () => {
+  beforeAll(async (done) => {
     await createConnection().catch(err => {throw new Error(err)});
+    done();
   });
 
-  afterAll(async () => {
+  afterAll(async (done) => {
     const testingConnection = getConnection();
     await testingConnection.close().catch(err => {throw new Error(err)});
+    done();
   });
 }
