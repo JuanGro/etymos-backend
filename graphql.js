cabb13c6fe7e004417be89860a2dd9b7c0371835
Juan Guerrero <ing.juangro@gmail.com>
2021-06-23 18:55:16 -0500

    Add deploy config

---
+++ graphql.js
@@ -0,0 +1,27 @@
+// graphql.js
+
+import { ApolloServer, gql } from 'apollo-server-lambda';
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: {
+    endpoint: '/dev/graphql',
+  },
+});
+
+export const graphqlHandler = server.createHandler();
