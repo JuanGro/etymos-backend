463ce5594df5062de47c079e761e6de3d82039ab
Juan Guerrero <ing.juangro@gmail.com>
2021-06-22 20:05:31 -0500

    Add more constants

--- src/seeds/factories/reference.factory.ts
+++ src/seeds/factories/reference.factory.ts
@@ -1,12 +1,12 @@
 import { define } from "typeorm-seeding";
-import { FAKER_ELEMENTS_NUMBER_L, FAKER_ELEMENTS_NUMBER_M, FAKER_ELEMENTS_NUMBER_S } from "../../config/constants";
+import { FAKER_ELEMENTS_NUMBER_L, FAKER_ELEMENTS_NUMBER_M, FAKER_ELEMENTS_NUMBER_S, FAKER_ELEMENTS_NUMBER_XXL } from "../../config/constants";
 import { Reference } from "../../models/Reference";
 
 define(Reference, (faker: Faker.FakerStatic) => {
   const reference = new Reference();
   reference.author = faker.name.firstName() + faker.name.lastName();
   reference.title = faker.random.words(FAKER_ELEMENTS_NUMBER_M);
-  reference.publicationYear = faker.random.number(9999).toString();
+  reference.publicationYear = faker.random.number(FAKER_ELEMENTS_NUMBER_XXL).toString();
   reference.publicationPlace = faker.random.alphaNumeric(FAKER_ELEMENTS_NUMBER_L);
   reference.publishingCompany = faker.random.words(FAKER_ELEMENTS_NUMBER_S);
   reference.active = faker.random.boolean();
--- src/seeds/factories/version.factory.ts
+++ src/seeds/factories/version.factory.ts
@@ -1,12 +1,12 @@
 import { define } from "typeorm-seeding";
-import { FAKER_ELEMENTS_NUMBER_M } from "../../config/constants";
+import { FAKER_ELEMENTS_NUMBER_M, FAKER_ELEMENTS_NUMBER_XL } from "../../config/constants";
 import { Version } from "../../models/Version";
 
 define(Version, (faker: Faker.FakerStatic) => {
   const version = new Version();
-  version.version = `${faker.random.number(99)}.${faker.random.number(
-    99
-  )}.${faker.random.number(99)}`;
+  version.version = `${faker.random.number(FAKER_ELEMENTS_NUMBER_XL)}.${faker.random.number(
+    FAKER_ELEMENTS_NUMBER_XL
+  )}.${faker.random.number(FAKER_ELEMENTS_NUMBER_XL)}`;
   version.description = faker.random.words(FAKER_ELEMENTS_NUMBER_M);
   version.maintenance = faker.random.boolean();
   version.active = faker.random.boolean();
